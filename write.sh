e4c133407601239880d7a8aef2f36e616d863f8a
Ryan Fortner <pinetmedia@gmail.com>
2021-08-22 13:20:40 -0400

    Update write.sh

--- write.sh
+++ write.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-cd /home/pi/raspbian-addons/debian/
-sudo su
-EMAIL="$(cat /home/pi/email)"
-GPGPASS="$(cat /home/pi/gpgpass)"
-rm InRelease Release Release.gpg Packages Packages.gz
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
-apt-ftparchive release . > Release
-gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" -abs -o - Release > Release.gpg
-gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" --clearsign -o - Release > InRelease
-echo "done"
-exit 0
+#cd /home/pi/raspbian-addons/debian/
+#EMAIL="$(cat /home/pi/email)"
+#GPGPASS="$(cat /home/pi/gpgpass)"
+#rm InRelease Release Release.gpg Packages Packages.gz
+#dpkg-scanpackages --multiversion . > Packages
+#gzip -k -f Packages
+#apt-ftparchive release . > Release
+#gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" -abs -o - Release > Release.gpg
+#gpg --default-key "${EMAIL}" --batch --pinentry-mode="loopback" --passphrase="$GPGPASS" --clearsign -o - Release > InRelease
+#echo "done"
+#exit 0
