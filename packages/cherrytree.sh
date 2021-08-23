cf6069ee35ed22a2159aa58bb09a288fe83703ac
Tony <1462668901@qq.com>
2021-08-23 08:48:56 +0800

    Update cherrytree.sh
    for test

--- packages/cherrytree.sh
+++ packages/cherrytree.sh
@@ -4,4 +4,14 @@ PKGDIR="/home/pi/raspbian-addons/debian/pool/"
 #the -3 at the end of the version number causes this to not work
 #LATEST=`curl -s https://api.github.com/repos/giuspen/cherrytree/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 #https://launchpad.net/~giuspen/+archive/ubuntu/ppa/+files/cherrytree_0.99.40-3_arm64.deb
-echo "does not work yet."
+#echo "does not work yet."
+if [ ! -f "/etc/apt/sources.list.d/cherrytree.list" ]; then
+	echo "cherrytree.list does not exist. adding repo..."
+  echo "deb http://ppa.launchpad.net/giuspen/ppa/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/cherrytree.list
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A98E8BBCABCF6A49A5DA2F43B8668B055FE1EFE4
+	sudo apt update
+fi
+echo "cherrytree.list exists. continuing..."
+sudo apt update
+apt download cherrytree:arm64
+mv cherrytree_* $PKGDIR
