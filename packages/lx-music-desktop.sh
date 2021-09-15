0ad30d8e2c53eb365f89fda86a88d74fe0b95730
Ryan Fortner <71036629+chunky-milk@users.noreply.github.com>
2021-09-15 11:28:00 -0400

    Create lx-music-desktop.sh

---
+++ packages/lx-music-desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#!/bin/bash
+PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+LATEST=`curl -s https://api.github.com/repos/lyswhut/lx-music-desktop/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/lyswhut/lx-music-desktop/releases/latest \
+  | grep browser_download_url \
+  | grep 'armv7l.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o $PKGDIR/lx-music-deskop-$LATEST-armhf.deb
+
+curl -s https://api.github.com/repos/lyswhut/lx-music-desktop/releases/latest \
+  | grep browser_download_url \
+  | grep 'arm64.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o $PKGDIR/lx-music-desktop-$LATEST-arm64.deb
