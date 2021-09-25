474ba281a892a7fa5d4da89b5a47483a0986125a
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-09-24 20:20:58 -0400

    Update box64.sh

--- packages/box64.sh
+++ packages/box64.sh
@@ -3,8 +3,8 @@ PKGDIR="/home/pi/raspbian-addons/debian/pool/"
 
 if [ ! -f "/etc/apt/sources.list.d/box64.list" ]; then
 	echo "box64.list does not exist. adding repo..."
-	sudo wget https://box64.armlinux.ml/box64.list -O /etc/apt/sources.list.d/box64.list
-	wget -qO- https://box64.armlinux.ml/KEY.gpg | sudo apt-key add -
+	sudo wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
+	wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | sudo apt-key add -
 	sudo apt update
 fi
 echo "box64.list exists. continuing..."
