78c5008386f06f8c4c3fd4b88ff2b6cf35e4a376
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-09-24 20:22:03 -0400

    Update box86.sh

--- packages/box86.sh
+++ packages/box86.sh
@@ -3,8 +3,8 @@ PKGDIR="/home/pi/raspbian-addons/debian/pool/"
 
 if [ ! -f "/etc/apt/sources.list.d/box86.list" ]; then
 	echo "box86.list does not exist. adding repo..."
-	sudo wget https://box86.armlinux.ml/box86.list -O /etc/apt/sources.list.d/box86.list
-	wget -qO- https://box86.armlinux.ml/KEY.gpg | sudo apt-key add -
+	sudo wget https://ryanfortner.github.io/box86-debs/box86.list -O /etc/apt/sources.list.d/box86.list
+	wget -qO- https://ryanfortner.github.io/box86-debs/KEY.gpg | sudo apt-key add -
 	sudo apt update
 fi
 echo "box86.list exists. continuing..."
