326a87163cb551fd88df609d945ae21205e8ac3f
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:43:07 -0400

    Update windows95.sh

--- packages/windows95.sh
+++ packages/windows95.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/felixrieseberg/windows95/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/felixrieseberg/windows95/releases/latest \
   | grep browser_download_url \
