cefdbe9fac002c086602572a93c397356aba70da
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-19 20:49:26 -0400

    Update browsh.sh

--- packages/browsh.sh
+++ packages/browsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 # browsh
 LATEST=`curl -s https://api.github.com/repos/browsh-org/browsh/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/browsh-org/browsh/releases/latest \
