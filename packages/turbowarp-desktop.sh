f80f55680198fa19ac48190eaa80d7a8d5ab42b7
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:40:02 -0400

    Update turbowarp-desktop.sh

--- packages/turbowarp-desktop.sh
+++ packages/turbowarp-desktop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/TurboWarp/desktop/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 wget https://github.com/TurboWarp/desktop/releases/download/v${LATEST}/TurboWarp-linux-arm64-${LATEST}.deb -O $PKGDIR/turbowarp-desktop_${LATEST}_arm64.deb
 wget https://github.com/TurboWarp/desktop/releases/download/v${LATEST}/TurboWarp-linux-armv7l-${LATEST}.deb -O $PKGDIR/turbowarp-desktop_${LATEST}_armhf.deb
