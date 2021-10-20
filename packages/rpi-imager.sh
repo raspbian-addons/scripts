f336917b22a22bcb8c3d7bbfb7c2b0be6fc93f8d
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:34:27 -0400

    Update rpi-imager.sh

--- packages/rpi-imager.sh
+++ packages/rpi-imager.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/raspberrypi/rpi-imager/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 wget http://archive.raspberrypi.org/debian/pool/main/r/rpi-imager/rpi-imager-dbgsym_${LATEST}_arm64.deb -O $PKGDIR/rpi-imager-dbgsym_${LATEST}_arm64.deb
 wget http://archive.raspberrypi.org/debian/pool/main/r/rpi-imager/rpi-imager-dbgsym_${LATEST}_armhf.deb -O $PKGDIR/rpi-imager-dbgsym_${LATEST}_armhf.deb
