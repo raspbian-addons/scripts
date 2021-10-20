af7ad68ff865c8be808e953384cb53c960d39bd1
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:37:15 -0400

    Update sysbench.sh

--- packages/sysbench.sh
+++ packages/sysbench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 #LATEST=`curl -s https://api.github.com/repos/PapirusDevelopmentTeam/papirus-icon-theme/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 #wget https://launchpad.net/~papirus/+archive/ubuntu/papirus/+files/papirus-icon-theme_$LATEST-5385+pkg21~ubuntu21.10.1_all.deb -O $PKGDIR/papirus-icon-theme-$LATEST.deb
 
