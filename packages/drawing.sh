fdaed081f3f2c71a393cfdd5c62b2fff18ebbf52
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 07:55:56 -0400

    Update drawing.sh

--- packages/drawing.sh
+++ packages/drawing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 # drawing
 LATEST=`curl -s https://api.github.com/repos/maoschanz/drawing/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 wget https://launchpad.net/~cartes/+archive/ubuntu/drawing/+files/drawing_$LATEST-0ubuntu1~hirsute_arm64.deb -O $PKGDIR/drawing-$LATEST-arm64.deb
