7093062651c0edac6bdde91db2e0a260c097ceed
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-19 20:49:14 -0400

    Update box86.sh

--- packages/box86.sh
+++ packages/box86.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 
 if [ ! -f "/etc/apt/sources.list.d/box86.list" ]; then
 	echo "box86.list does not exist. adding repo..."
