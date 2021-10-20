4dad499bf3e140da4932a6199415235c63a06fe8
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:41:28 -0400

    Update v2raya.sh

--- packages/v2raya.sh
+++ packages/v2raya.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/v2rayA/v2rayA/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 wget https://github.com/v2rayA/v2rayA/releases/download/v${LATEST}/installer_debian_armhf_v${LATEST}.deb -O $PKGDIR/v2raya-${LATEST}-armhf.deb
 wget https://github.com/v2rayA/v2rayA/releases/download/v${LATEST}/installer_debian_aarch64_v${LATEST}.deb -O $PKGDIR/v2raya-${LATEST}-arm64.deb
