4fc4269e8611e2557de0b4c75865be9984fb63f3
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-19 20:48:36 -0400

    Update bleachbit.sh

--- packages/bleachbit.sh
+++ packages/bleachbit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 #LATEST=`curl -s https://api.github.com/repos/bleachbit/bleachbit/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 #wget https://download.bleachbit.org/bleachbit_$LATEST-0_all_debian10.deb -O $PKGDIR/bleachbit_$LATEST_all.deb
 echo "this script doesn't work yet"
