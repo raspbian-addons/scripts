127a6c5efa05c45c03907debbdeb2f138e22b5f5
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:28:18 -0400

    Update pbgopy.sh

--- packages/pbgopy.sh
+++ packages/pbgopy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/nakabonne/pbgopy/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/nakabonne/pbgopy/releases/latest \
   | grep browser_download_url \
