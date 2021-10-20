26cdda1ff742fae804892b53469211b461319e1e
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:35:36 -0400

    Update stl-thumb-kde.sh

--- packages/stl-thumb-kde.sh
+++ packages/stl-thumb-kde.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/unlimitedbacon/stl-thumb/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 wget https://github.com/unlimitedbacon/stl-thumb/releases/download/v${LATEST}/stl-thumb-kde_${LATEST}_arm64.deb -O $PKGDIR/stl-thumb-kde_${LATEST}_arm64.deb
 wget https://github.com/unlimitedbacon/stl-thumb/releases/download/v${LATEST}/stl-thumb-kde_${LATEST}_armhf.deb -O $PKGDIR/stl-thumb-kde_${LATEST}_armhf.deb
