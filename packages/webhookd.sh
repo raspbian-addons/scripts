fb693e333e5b2d4285168ae2208eefe8c347e523
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-20 08:42:20 -0400

    Update webhookd.sh

--- packages/webhookd.sh
+++ packages/webhookd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/ncarlier/webhookd/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
 
 wget https://packages.azlux.fr/debian/pool/main/w/webhookd/webhookd_${LATEST}_arm64.deb -O $PKGDIR/webhookd-${LATEST}-arm64.deb
