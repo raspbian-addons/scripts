a362e07fb87d575ad61ef041152d322e82c861f7
Ryan Fortner <pinetmedia@gmail.com>
2021-10-20 08:32:35 -0400

    remove pt python packages

--- packages/pi-top-4.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PKGDIR="/root/raspbian-addons/debian/pool/"
-LATEST=`curl -s https://api.github.com/repos/pi-top/pi-topOS-Core-Packages/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
-wget https://github.com/pi-top/pi-topOS-Core-Packages/releases/download/v${LATEST}/pi-top-4_${LATEST}_all.deb -O $PKGDIR/pi-top-4_${LATEST}_all.deb
--- packages/python3-pitop-doc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
-LATEST=`curl -s https://api.github.com/repos/pi-top/pi-top-Python-SDK/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
-wget https://github.com/pi-top/pi-top-Python-SDK/releases/download/v${LATEST}/python3-pitop-doc_${LATEST}_all.deb -O $PKGDIR/python3-pitop-doc_${LATEST}_all.deb
