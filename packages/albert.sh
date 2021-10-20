611a3f98108660429b7a68bcd6c869e7ea98d5e1
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-19 20:47:44 -0400

    Update albert.sh

--- packages/albert.sh
+++ packages/albert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PKGDIR="/home/pi/raspbian-addons/debian/pool/"
+PKGDIR="/root/raspbian-addons/debian/pool/"
 if [ ! -f "/etc/apt/sources.list.d/home:manuelschneid3r.list" ]; then
 	echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/Raspbian_10/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 	curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/Raspbian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
