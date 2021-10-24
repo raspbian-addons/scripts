260d28c947668747b61be484cf8c4a0cbd31208c
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-24 18:19:18 -0400

    Create electron-fiddle.sh

---
+++ autoupdate/packages/electron-fiddle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PKGDIR="/root/raspbian-addons/debian/pool/"
+LATEST=`curl -s https://api.github.com/repos/electron/fiddle/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/electron/fiddle/releases/latest \
+  | grep browser_download_url \
+  | grep 'armhf.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o $PKGDIR/electron-fiddle-$LATEST-armhf.deb
+
+curl -s https://api.github.com/repos/electron/fiddle/releases/latest \
+  | grep browser_download_url \
+  | grep 'arm64.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o $PKGDIR/electron-fiddle-$LATEST-arm64.deb
