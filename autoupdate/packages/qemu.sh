537f1a7dde62bc85cb3b6e82bcb7d95f0262095c
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-10-28 13:04:10 -0400

    update username in qemu.sh

--- autoupdate/packages/qemu.sh
+++ autoupdate/packages/qemu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 PKGDIR="/root/raspbian-addons/debian/pool/"
-LATEST=`curl -s https://api.github.com/repos/chunky-milk/qemu-arm-builds/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
-curl -s https://api.github.com/repos/chunky-milk/qemu-arm-builds/releases/latest \
+LATEST=`curl -s https://api.github.com/repos/ryanfortner/qemu-arm-builds/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/ryanfortner/qemu-arm-builds/releases/latest \
   | grep browser_download_url \
   | grep 'armhf.deb"' \
   | cut -d '"' -f 4 \
   | xargs -n 1 curl -L -o $PKGDIR/qemu-$LATEST-armhf.deb
 
-curl -s https://api.github.com/repos/chunky-milk/qemu-arm-builds/releases/latest \
+curl -s https://api.github.com/repos/ryanfortner/qemu-arm-builds/releases/latest \
   | grep browser_download_url \
   | grep 'arm64.deb"' \
   | cut -d '"' -f 4 \
