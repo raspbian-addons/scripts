e64bad5e26299f77269da46ad51c96bc7aa476dc
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-03 13:13:05 -0400

    Update main.sh

--- autoupdate/main.sh
+++ autoupdate/main.sh
@@ -18,7 +18,7 @@ app="$(echo $1)"
 #FILES=$(bash -c "ls $PKGDIR | grep $app")
 #echo "enter app to update (use deb package name): "
 #read app
-rm -f $PKGDIR/${app}_*
+rm $PKGDIR/${app}_* || rm $PKGDIR/${app}-*
 bash packages/${app}.sh
 #bash write.sh
 echo "Make sure to run write.sh, or the repository will break!"
