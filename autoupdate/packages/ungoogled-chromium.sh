689c73d479dfa0b43c0c8e0334d5b96ed3ef752d
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-07 09:24:12 -0500

    Update ungoogled-chromium.sh

--- autoupdate/packages/ungoogled-chromium.sh
+++ autoupdate/packages/ungoogled-chromium.sh
@@ -2,8 +2,8 @@
 PKGDIR="/root/raspbian-addons/debian/pool/"
 echo "https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/"
 if [ ! -f "/etc/apt/sources.list.d/home-ungoogled_chromium.list" ]; then
-  echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
-  curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+  echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Bullseye/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+  curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Bullseye/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
   sudo apt update
 fi
 echo "home-ungoogled_chromium.list exists. continuing..."
