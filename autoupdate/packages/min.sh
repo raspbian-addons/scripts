648cc7fce7e1232bc1b795bd7f329333e6cc0c31
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-15 07:13:37 -0500

    Update min.sh

--- autoupdate/packages/min.sh
+++ autoupdate/packages/min.sh
@@ -3,12 +3,13 @@ PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/ryanfortner/minbrowser-arm-builds/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/ryanfortner/minbrowser-arm-builds/releases/latest \
   | grep browser_download_url \
-  | grep 'arm64.deb"' \
-  | cut -d '"' -f 4 \
-  | xargs -n 1 curl -L -o $PKGDIR/min-$LATEST-arm64.deb
-
-curl -s https://api.github.com/repos/ryanfortner/minbrowser-arm-builds/releases/latest \
-  | grep browser_download_url \
   | grep 'armhf.deb"' \
   | cut -d '"' -f 4 \
   | xargs -n 1 curl -L -o $PKGDIR/min-$LATEST-armhf.deb
+
+LATEST=`curl -s https://api.github.com/repos/minbrowser/min/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -s https://api.github.com/repos/minbrowser/min/releases/latest \
+  | grep browser_download_url \
+  | grep 'arm64.deb"' \
+  | cut -d '"' -f 4 \
+  | xargs -n 1 curl -L -o $PKGDIR/min-$LATEST-arm64.deb
