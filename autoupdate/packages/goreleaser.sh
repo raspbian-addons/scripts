e9071436922b574a13eb2d6b9144f5230b0b9217
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-11-24 13:41:39 -0500

    fix goreleaser armhf download

--- autoupdate/packages/goreleaser.sh
+++ autoupdate/packages/goreleaser.sh
@@ -3,7 +3,7 @@ PKGDIR="/root/raspbian-addons/debian/pool/"
 LATEST=`curl -s https://api.github.com/repos/goreleaser/goreleaser/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
 curl -s https://api.github.com/repos/goreleaser/goreleaser/releases/latest \
   | grep browser_download_url \
-  | grep 'armv6.deb"' \
+  | grep 'armhf.deb"' \
   | cut -d '"' -f 4 \
   | xargs -n 1 curl -L -o $PKGDIR/goreleaser-$LATEST-armhf.deb
 
