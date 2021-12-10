4b11e275bd388f15653361b3c43e6dbaab2eba6d
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-12-10 10:45:51 -0500

    Update au-rewrite.sh

--- au-rewrite.sh
+++ au-rewrite.sh
@@ -88,6 +88,36 @@ if [ "$GORELEASER_CURRENT" != "$GORELEASER_API" ]; then
 fi
 echo "goreleaser is up to date."
 
+echo "Updating hyperfine."
+HYPERFINE_API=`curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+HYPERFINE_DATAFILE="$HOME/dlfiles-data/hyperfine.txt"
+if [ ! -f "$HYPERFINE_DATAFILE" ]; then
+    echo "$HYPERFINE_DATAFILE does not exist."
+    echo "Grabbing the latest release from GitHub."
+    echo $HYPERFINE_API > $HYPERFINE_DATAFILE
+fi
+HYPERFINE_CURRENT="$(cat ${HYPERFINE_DATAFILE})"
+if [ "$HYPERFINE_CURRENT" != "$HYPERFINE_API" ]; then
+    echo "hyperfine isn't up to date. updating now..."
+    curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest \
+      | grep browser_download_url \
+      | grep 'armhf.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o hyperfine_${HYPERFINE_API}_armhf.deb || error "Failed to download hyperfine:armhf"
+
+    curl -s https://api.github.com/repos/sharkdp/hyperfine/releases/latest \
+      | grep browser_download_url \
+      | grep 'arm64.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o hyperfine_${HYPERFINE_API}_arm64.deb || error "Failed to download hyperfine:arm64"
+
+    mv hyperfine* $PKGDIR
+    echo "hyperfine downloaded successfully."
+fi
+echo "hyperfine is up to date."
+
+echo "Updating blockbench."
+
 echo "Writing packages."
 cd /root/raspbian-addons/debian
 for new_pkg in `ls pkgs_incoming`; do
