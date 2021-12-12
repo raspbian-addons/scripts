b67ad4d5517374c0e613e6f708a1fc01ddc55912
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-12-12 17:08:25 -0500

    Update au-rewrite.sh

--- au-rewrite.sh
+++ au-rewrite.sh
@@ -168,6 +168,26 @@ if [ "$WEBCORD_CURRENT" != "$WEBCORD_API" ]; then
 fi
 echo "webcord is up to date."
 
+echo "Updating gitea."
+GITEA_API=`curl -s https://api.github.com/repos/go-gitea/gitea/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+GITEA_DATAFILE="$HOME/dlfiles-data/gitea.txt"
+if [ ! -f "$GITEA_DATAFILE" ]; then
+    echo "$GITEA_DATAFILE does not exist."
+    echo "Grabbing the latest release from GitHub."
+    echo $GITEA_API > $GITEA_DATAFILE
+fi
+GITEA_CURRENT="$(cat ${GITEA_DATAFILE})"
+if [ "$GITEA_CURRENT" != "$GITEA_API" ]; then
+    echo "gitea isn't up to date. updating now..."
+    GITEA_API_NOV=`curl -s https://api.github.com/repos/go-gitea/gitea/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
+    wget https://github.com/ryanfortner/gitea-arm/raw/master/debs/gitea_${GITEA_API_NOV}_arm64.deb || error "Failed to download gitea:arm64"
+    wget https://github.com/ryanfortner/gitea-arm/raw/master/debs/gitea_${GITEA_API_NOV}_armhf.deb || error "Failed to download gitea:armhf"
+    mv gitea* $PKGDIR
+    echo $GITEA_API > $GITEA_DATAFILE
+    echo "gitea downloaded successfully."
+fi
+echo "gitea is up to date."
+
 echo "Writing packages."
 cd /root/raspbian-addons/debian
 for new_pkg in `ls pkgs_incoming`; do
