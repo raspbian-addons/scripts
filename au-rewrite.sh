f6db317f0ba0117389a27edb2163b6e8efd72ac8
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-12-23 11:18:59 -0500

    autoupdate: add cloudflared

--- au-rewrite.sh
+++ au-rewrite.sh
@@ -325,6 +325,34 @@ if [ "$GH_CURRENT" != "$GH_API" ]; then
 fi
 green "gh is up to date."
 
+status "Updating cloudflared."
+CLOUDFLARED_API=`curl -s --header --header "Authorization: token $token" https://api.github.com/repos/cloudflare/cloudflared/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+CLOUDFLARED_DATAFILE="$HOME/dlfiles-data/cloudflared.txt"
+if [ ! -f "$CLOUDFLARED_DATAFILE" ]; then
+    status "$CLOUDFLARED_DATAFILE does not exist."
+    status "Grabbing the latest release from GitHub."
+    echo $CLOUDFLARED_API > $CLOUDFLARED_DATAFILE
+fi
+CLOUDFLARED_CURRENT="$(cat ${CLOUDFLARED_DATAFILE})"
+if [ "$CLOUDFLARED_CURRENT" != "$CLOUDFLARED_API" ]; then
+    status "gh isn't up to date. updating now..."
+    curl -s --header --header "Authorization: token $token" https://api.github.com/repos/cloudflare/cloudflared/releases/latest \
+      | grep browser_download_url \
+      | grep 'arm64.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o cloudflared_${CLOUDFLARED_API}_arm64.deb || error "Failed to download cloudflared:arm64!"
+    curl -s --header --header "Authorization: token $token" https://api.github.com/repos/cloudflare/cloudflared/releases/latest \
+      | grep browser_download_url \
+      | grep 'arm.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o cloudflared_${CLOUDFLARED_API}_armhf.deb || error "Failed to download cloudflared:armhf!"
+
+    mv cloudflared* $PKGDIR
+    echo $CLOUDFLARED_API > $CLOUDFLARED_DATAFILE
+    green "cloudflared downloaded successfully."
+fi
+green "cloudflared is up to date."
+
 status "Writing packages."
 cd /root/raspbian-addons/debian
 for new_pkg in `ls pkgs_incoming`; do
