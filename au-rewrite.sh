b74772a146d5f2f39c8676c6781e0beacebe3052
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-12-25 16:49:09 -0500

    autoupdate: add grype package

--- au-rewrite.sh
+++ au-rewrite.sh
@@ -407,6 +407,29 @@ if [ "$FM_CURRENT" != "$FM_API" ]; then
 fi
 green "fm is up to date."
 
+status "Updating grype."
+GRYPE_API=`curl -s --header --header "Authorization: token $token" https://api.github.com/repos/anchore/grype/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+GRYPE_DATAFILE="$HOME/dlfiles-data/grype.txt"
+if [ ! -f "$GRYPE_DATAFILE" ]; then
+    status "$GRYPE_DATAFILE does not exist."
+    status "Grabbing the latest release from GitHub."
+    echo $GRYPE_API > $GRYPE_DATAFILE
+fi
+GRYPE_CURRENT="$(cat ${GRYPE_DATAFILE})"
+if [ "$GRYPE_CURRENT" != "$GRYPE_API" ]; then
+    status "grype isn't up to date. updating now..."
+    curl -s --header --header "Authorization: token $token" https://api.github.com/repos/anchore/grype/releases/latest \
+      | grep browser_download_url \
+      | grep 'arm64.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o grype_${GRYPE_API}_arm64.deb || error "Failed to download grype:arm64!"
+
+    mv grype* $PKGDIR
+    echo $GRYPE_API > $GRYPE_DATAFILE
+    green "grype downloaded successfully."
+fi
+green "grype is up to date."
+
 status "Writing packages."
 cd /root/raspbian-addons/debian
 for new_pkg in `ls pkgs_incoming`; do
