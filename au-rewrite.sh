6fe48693a8d7aff3b2bf50218a631157fa123f50
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2021-12-29 12:57:33 -0500

    autoupdate: add broot

--- au-rewrite.sh
+++ au-rewrite.sh
@@ -458,6 +458,43 @@ if [ "$MIN_CURRENT" != "$MIN_API" ]; then
 fi
 green "min is up to date."
 
+status "Updating broot."
+BROOT_API=`curl -s --header "Authorization: token $token" https://api.github.com/repos/minbrowser/broot/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'`
+BROOT_DATAFILE="$HOME/dlfiles-data/broot.txt"
+if [ ! -f "$BROOT_DATAFILE" ]; then
+    status "$BROOT_DATAFILE does not exist."
+    status "Grabbing the latest release from GitHub."
+    echo $BROOT_API > $BROOT_DATAFILE
+fi
+BROOT_CURRENT="$(cat ${BROOT_DATAFILE})"
+if [ "$BROOT_CURRENT" != "$BROOT_API" ]; then
+    status "broot isn't up to date. updating now..."
+    mkdir broot-tmp || error "Failed to make broot build dir."
+    cd broot-tmp || error "Failed to enter broot build dir."
+    mkdir deb-base || error "Failed to make deb base folder for broot."
+    mkdir -p deb-base/DEBIAN && mkdir -p deb-base/usr/bin
+    wget https://dystroy.org/broot/download/armv7-unknown-linux-gnueabihf/broot -O deb-base/usr/bin/broot || error "Failed to download broot binary"
+    chmod +x deb-base/usr/bin/broot || error "Failed to make broot binary executable"
+    echo "Package: broot
+Version: $new
+Section: net
+Priority: optional
+Architecture: ${BROOT_API}
+Depends: bash
+Maintainer: Ryan Fortner <ryankfortner@gmail.com>
+Description: A new way to see and navigate directory trees
+Homepage: https://dystroy.org/broot/
+Bugs: https://github.com/Canop/broot/issues" > deb-base/DEBIAN/control || error "Failed to make control file for broot"
+    chmod 755 deb-base/DEBIAN/*
+    dpkg-deb --build deb-base/ ../broot_${BROOT_API}_armhf.deb || error "failed to create broot deb file (armhf)!"
+    cd ../ && rm -rf broot-tmp
+    #wget https://packages.azlux.fr/debian/pool/main/b/broot/broot_${BROOT_API}_armhf.deb -O broot_${BROOT_API}_armhf.deb || error "Failed to download broot:armhf!"
+    mv broot* $PKGDIR
+    echo $BROOT_API > $BROOT_DATAFILE
+    green "broot downloaded successfully."
+fi
+green "broot is up to date."
+
 status "Writing packages."
 cd /root/raspbian-addons/debian
 for new_pkg in `ls pkgs_incoming`; do
